68841e3d096a056faaa939088c54b1632bdbb32f
Moritz Löchner <momo.loechner@gmail.com>
2022-04-10 18:12:55 +0200

    Cache App js

--- service-worker.js
+++ service-worker.js
@@ -4,6 +4,7 @@ const RUNTIME = 'runtime';
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   'index.html',
+  'app.js',
   'music.html',
   'feelings.html',
   'style.css',
